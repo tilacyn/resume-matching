e11108461373dcfd09832026b3e65cee3dfbb887
Maksim Kryuchkov <tilacyn@yandex.ru>
2019-11-13 10:34:39 +0300

    vacancy parser modified, restructured, not stable yet

---
+++ src/common/parser.py
@@ -0,0 +1,26 @@
+junk_subs = {
+    '\n',
+    '\t',
+    '\r',
+    '\x95'
+}
+
+
+def make_prefix_by_special_field(s):
+    return s + ':'
+
+
+def related(data, field):
+    return data.startswith(make_prefix_by_special_field(field)) or data.startswith(field)
+
+def try_add_field(data, field_map):
+    for key in field_map:
+        if related(data, key):
+            value_start_index = len(key) + 2
+            field_map[key] = data[value_start_index:]
+
+def format(data):
+    result = data
+    for junk in junk_subs:
+        result = result.replace(junk, "")
+    return result.strip()
--- src/run.py
+++ src/run.py
@@ -1,8 +1,12 @@
-from resumes.fetch.resume_fetcher import JobSpiderResumeFetcher
-from vacancy_fetcher import JobSpiderVacancyFetcher
-from resumes.parse.job_spider_parser import JobSpiderHTMLResumeParser
-import resumes.parse.job_spider_parser as sp
 from os import listdir
+from vacancies.vacancy import Vacancy
+
+import resumes.job_spider_resume_parser as sp
+from resumes.job_spider_resume_parser import JobSpiderHTMLResumeParser
+from resumes.resume import Resume
+from resumes.resume_fetcher import JobSpiderResumeFetcher
+from vacancies.job_spider_vacancy_parser import JobSpiderHTMLVacancyParser
+from vacancies.vacancy_fetcher import JobSpiderVacancyFetcher
 
 
 def fetch_resumes():
@@ -28,22 +32,36 @@ def print_resume(resume):
 
 def parse_resumes():
     resumes = []
-    for fname in listdir('resumes'):
+    observed_resumes = listdir('resumes')[:5]
+    for fname in observed_resumes:
         parser = JobSpiderHTMLResumeParser()
         print(fname)
         f = open('resumes/' + fname)
         parser.feed(f.read())
 
-        resume = parser.get_resume()
-        resume['id'] = fname
-        resumes.append(resume)
+        resume_field_map = parser.get_resume()
+        resume_field_map['id'] = fname
+        resumes.append(Resume(resume_field_map))
 
     return resumes
 
+def parse_vacancies():
+    vacancies = []
+    observed_vacancies = listdir('vacancies')[:5]
+    for fname in observed_vacancies:
+        parser = JobSpiderHTMLVacancyParser()
+        print(fname)
+        f = open('vacancies/' + fname)
+        parser.feed(f.read())
 
-fetch_resumes()
+        fmap, body = parser.get_vacancy()
+        vacancies.append(Vacancy(fmap, body, fname))
+
+
+
+#fetch_resumes()
 
 resumes = parse_resumes()
 
-for r in resumes[:20]:
-    print_resume(r)
+#for r in resumes:
+#    print_resume(r)
